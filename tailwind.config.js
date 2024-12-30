d847d3d5b61a943f534100f9cdadd137c4385b09
aniqatc <aniqatc97@gmail.com>
2024-12-30 15:20:38 -0500

    install fonts, dependencies, delete placeholder content, clear styling etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,49 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const plugin = require('tailwindcss/plugin');
 
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Reddit Sans",
+          "system-ui",
+          "-apple-system",
+          "Nunito",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "sans-serif"
+        ],
+        serif: [
+          "Source Serif 4",
+          "Georgia",
+          "Cambria",
+          "Times New Roman",
+          "Times",
+          "serif"
+        ],
+        mono: [
+          "Reddit Mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace"
+        ],
+      },
+    },
+  },
+  plugins: [plugin(({ addVariant }) => {
+    addVariant('sepia', '.sepia &')
+    addVariant('ocean', '.ocean &')
+  })
+  ],
+}
