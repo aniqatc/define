6d1e541d98614daa5a18f73c02f74d1fa94dede5
aniqatc <aniqatc97@gmail.com>
2025-01-01 06:02:25 -0500

    add disabled volume icon for words that dont have audio

--- src/components/grid/Word.js
+++ src/components/grid/Word.js
@@ -1,4 +1,4 @@
-import { Volume2 } from 'lucide-react';
+import { Volume2, VolumeOff } from 'lucide-react';
 import BaseCard from './BaseCard';
 
 function Word({ data }) {
@@ -12,7 +12,11 @@ function Word({ data }) {
         <BaseCard className="px-4 py-3 w-full flex flex-col gap-1">
             <h2 className="text-2xl font-semibold capitalize">{data.word}</h2>
             <div className="flex items-center gap-2">
-                <button className="p-1 -m-1 text-slate-600 dark:text-slate-300"><Volume2 strokeWidth={2.2} size={20} onClick={handleAudio}/></button>
+                <button className="p-1 -m-1 text-slate-600 dark:text-slate-300" onClick={handleAudio}>
+                    {!data.phonetics[0].audio
+                        ? <VolumeOff strokeWidth={2.2} size={20} className="text-gray-400" />
+                        : <Volume2 strokeWidth={2.2} size={20} />}
+                </button>
                 <span className="italic text-sm text-slate-700 dark:text-slate-400">{data.phonetic || data.phonetics.find(phonetic => phonetic.text)?.text}</span>
             </div>
         </BaseCard>
